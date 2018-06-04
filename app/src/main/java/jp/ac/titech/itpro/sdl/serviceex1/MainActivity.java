e6622c52267a623c9a714e8d27a2337bb2164c35
Erwin Wu <32383242+erwinwu211@users.noreply.github.com>
2018-06-04 13:32:40 +0900

    add Test3
    add Test3 button for broadcast

--- app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/MainActivity.java
@@ -1,13 +1,21 @@
 package jp.ac.titech.itpro.sdl.serviceex1;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "MainActivity";
+    private final static int REQCODE_TEST3 = 3333;
+
+    private BroadcastReceiver bcReceiver;
+    private IntentFilter bcScanFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,28 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate in " + Thread.currentThread());
         setContentView(R.layout.activity_main);
 
+        bcReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d(TAG, "onReceive in " + Thread.currentThread());
+                String action = intent.getAction();
+                if (action == null) return;
+                switch (action) {
+                case TestService3.ACTION_ANSWER:
+                    Log.d(TAG, "bcReceiver in " + Thread.currentThread());
+                    String answer = intent.getStringExtra(TestService3.EXTRA_ANSWER);
+                    Log.d(TAG, "answer = " + answer);
+                    Toast.makeText(context, "answer = " + answer,
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    Log.d(TAG, "bcReceiver [unexpected action] in " + Thread.currentThread());
+                    break;
+                }
+            }
+        };
+        bcScanFilter = new IntentFilter();
+        bcScanFilter.addAction(TestService3.ACTION_ANSWER);
     }
 
     public void pressTest1(View v) {
@@ -23,6 +53,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void pressTest2(View v) {
         testService2();
+    }
+
+    public void pressTest3(View v) {
+        testService3();
     }
 
     private void testService1() {
@@ -39,4 +73,24 @@ public class MainActivity extends AppCompatActivity {
         startService(intent);
     }
 
+    private void testService3() {
+        Log.d(TAG, "testService3 in " + Thread.currentThread());
+        Intent intent = new Intent(this, TestService3.class);
+        intent.putExtra(TestService3.EXTRA_MYARG, "Hello, Service3");
+        startService(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        registerReceiver(bcReceiver, bcScanFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        unregisterReceiver(bcReceiver);
+    }
 }
---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/TestService3.java
@@ -0,0 +1,45 @@
+package jp.ac.titech.itpro.sdl.serviceex1;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class TestService3 extends IntentService {
+    private final static String TAG = "TestService3";
+    public final static String EXTRA_MYARG = "MYARG";
+    public final static String ACTION_ANSWER = "ANSWER";
+    public final static String EXTRA_ANSWER = "ANSWER";
+
+    TestService3() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "onHandleIntent in " + Thread.currentThread());
+        Log.d(TAG, "myarg = " + intent.getStringExtra(EXTRA_MYARG));
+        try {
+            Thread.sleep(3333);
+            Intent answerIntent = new Intent();
+            answerIntent.setAction(ACTION_ANSWER);
+            answerIntent.putExtra(EXTRA_ANSWER, new String("Hello world, 123"));
+            sendBroadcast(answerIntent);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate in " + Thread.currentThread());
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy in " + Thread.currentThread());
+        super.onDestroy();
+    }
+}
+
